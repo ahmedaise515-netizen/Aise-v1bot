de218a0da56bce732f5fed8ad0dddba63072a9a2
ahmedaise515-netizen <ahmedaise515@gmail.com>
2026-06-12 23:29:53 +0300

    Add bot configuration settings in config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // معلومات البوت
+    botName: '『 Aɪsᴀ v1 』🩸',
+    botNumber: '201142182793',
+    ownerNumber: '201142182793',
+    ownerName: 'عقاب الأصلي',
+    channelId: '120363426553571462@newsletter',
+    channelName: 'Aisa',
+
+    // كود الربط 8 حروف وارقام
+    pairCode: 'AISA1234',
+
+    // بريفيكس البوت
+    prefix: '!',
+
+    // الرسالة اللي تحت كل قسم
+    footer: '*الـعـدالـة سـوف تـتـحـقـق مـهـمـا كـلـف الأمـر.*'
+}
